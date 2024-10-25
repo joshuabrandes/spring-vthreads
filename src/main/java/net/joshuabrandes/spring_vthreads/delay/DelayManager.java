c1731957f2da7e98f4613b8f7675f14115e79b99
joshuabrandes <47532061+joshuabrandes@users.noreply.github.com>
2024-10-26 00:29:53 +0200

    finishing first example

--- src/main/java/net/joshuabrandes/spring_vthreads/delay/DelayManager.java
+++ src/main/java/net/joshuabrandes/spring_vthreads/delay/DelayManager.java
@@ -1,21 +1,23 @@
 package net.joshuabrandes.spring_vthreads.delay;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @ResponseBody
 class DelayController {
 
     @PostMapping("/delay")
-    public String delay() {
-        try {
-            Thread.sleep(10_000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return "Delayed!";
+    public ResponseEntity<String> delay() {
+        var url = "https://httpstat.us/200?sleep=5000";
+
+        return ResponseEntity.ok()
+                .body(new RestTemplate()
+                        .getForEntity(url, String.class)
+                        .getBody());
     }
 }
