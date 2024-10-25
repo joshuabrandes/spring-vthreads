5608f27615882825bc29c04c427df1edb391902e
joshuabrandes <47532061+joshuabrandes@users.noreply.github.com>
2024-10-26 01:15:23 +0200

    adding an implementation using WebClient instead of RestTemplate

--- src/main/java/net/joshuabrandes/spring_vthreads/delay/DelayManager.java
+++ src/main/java/net/joshuabrandes/spring_vthreads/delay/DelayManager.java
@@ -1,14 +1,18 @@
 package net.joshuabrandes.spring_vthreads.delay;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @ResponseBody
 class DelayController {
+
+    final WebClient webClient = WebClient.create();
 
     @PostMapping("/delay")
     public ResponseEntity<String> delay() {
@@ -18,5 +22,15 @@ class DelayController {
                 .body(new RestTemplate()
                         .getForEntity(url, String.class)
                         .getBody());
+
+        /*
+        return ResponseEntity.ok()
+                .body(webClient.get()
+                        .uri(url)
+                        .header("Accept", "application/json")
+                        .retrieve()
+                        .bodyToMono(String.class)
+                        .block());
+         */
     }
 }
