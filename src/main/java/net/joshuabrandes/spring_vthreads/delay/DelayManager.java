901857c039eaa01d675ba8635cd379b86878222a
joshuabrandes <47532061+joshuabrandes@users.noreply.github.com>
2024-10-26 01:16:03 +0200

    optimize imports

--- src/main/java/net/joshuabrandes/spring_vthreads/delay/DelayManager.java
+++ src/main/java/net/joshuabrandes/spring_vthreads/delay/DelayManager.java
@@ -1,6 +1,5 @@
 package net.joshuabrandes.spring_vthreads.delay;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
