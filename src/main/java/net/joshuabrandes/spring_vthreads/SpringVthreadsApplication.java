e2e4060a13560832b0de1ef0fde3e6d3a8c525e0
Joshua Brandes <47532061+joshuabrandes@users.noreply.github.com>
2024-10-25 21:19:12 +0200

    first commit

---
+++ src/main/java/net/joshuabrandes/spring_vthreads/SpringVthreadsApplication.java
@@ -0,0 +1,13 @@
+package net.joshuabrandes.spring_vthreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVthreadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVthreadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/joshuabrandes/spring_vthreads/SpringVthreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.joshuabrandes.spring_vthreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVthreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
