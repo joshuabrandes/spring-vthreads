4ce3ca9edc713ef92ec097012943a01aebf99ddc
joshuabrandes <47532061+joshuabrandes@users.noreply.github.com>
2024-10-29 08:57:58 +0100

    adds more CPU intensive endpoint

--- src/main/java/net/joshuabrandes/spring_vthreads/delay/DelayManager.java
+++ src/main/java/net/joshuabrandes/spring_vthreads/delay/DelayManager.java
@@ -4,14 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
+import java.util.List;
+
 @RestController
 @ResponseBody
 @RequiredArgsConstructor
-class DelayController {
+class EndpointController {
 
     final ApplicationEventPublisher eventPublisher;
 
@@ -35,5 +39,37 @@ class DelayController {
                         .bodyToMono(String.class)
                         .block());
          */
+    }
+
+    @PostMapping(path = "/sort", consumes = "application/json")
+    public ResponseEntity<List<Integer>> sort(@RequestBody List<Integer> numbers) {
+        eventPublisher.publishEvent("Sorting numbers...");
+
+        return ResponseEntity.ok()
+                .body(SortingService.sort(numbers, true));
+    }
+}
+
+class SortingService {
+
+    private SortingService() {
+    }
+
+    public static List<Integer> sort(List<Integer> numbers, boolean delay) {
+        var sorted = numbers.stream()
+                .sorted()
+                .toList();
+
+        if (delay) {
+            // randomize list and sort again
+            for (int i = 0; i < 500; i++) {
+                Collections.shuffle(sorted);
+                sorted = sorted.stream()
+                        .sorted()
+                        .toList();
+            }
+        }
+
+        return sorted;
     }
 }
--- src/main/java/net/joshuabrandes/spring_vthreads/logger/LogService.java
+++ src/main/java/net/joshuabrandes/spring_vthreads/logger/LogService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-public class LogService {
+class LogService {
 
     int count = 0;
 
